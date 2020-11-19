a69b8ec48ae0e552c8c5cb1b2ce034f7999cfd1a
baiaimin <baiaimin@yuzhilin.net.cn>
2020-11-19 13:45:26 +0800

    添加mode后可以获取各个环境

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import "./index.css";
 import router from "./router/index";
 import store from "./store/index";
 
-console.log(import.meta.env.VITE_APP_CONTEXT); // 打印出 lsbdb
-console.log(import.meta.env.VITE_APP_AGE); // 打印出 lsbdb
-console.log(import.meta.env.VITE_APP_NAME); // 打印出 lsbdb
+// console.log(import.meta.env.VITE_APP_CONTEXT); // 打印出 lsbdb
+// console.log(import.meta.env.VITE_APP_AGE); // 打印出 lsbdb
+// console.log(import.meta.env.VITE_APP_NAME); // 打印出 lsbdb
+
+
+const add = process.env
+
+add.VITE_APP_NAME = import.meta.env.VITE_APP_NAME
+console.log(add.VITE_APP_NAME);
+
+
+// console.log(add.indexOf("HUANGBIAO123") );
 
 createApp(App).use(router).use(store).mount("#app");
